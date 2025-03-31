9b511aaecd18fada551e651ff34c66cd36ebfa4f
Masha Drapkin <masha.dra@gmail.com>
2025-03-31 14:52:37 -0400

    adding animal table

--- base.sql
+++ base.sql
@@ -3,4 +3,14 @@ create database adoptables;
 
 use adoptables;
 
-create table 
+drop table if exists animal;
+create table animal(
+	animal_id int primary key,
+    type varchar(50) not null,
+    breed varchar(50),
+    name varchar(50) not null,
+    dot date,
+    kidFriendly tinyint not null,
+    pottyTrained tinyint,
+    adoptionPrice varchar(50)
+);
